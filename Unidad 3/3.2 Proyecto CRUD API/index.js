8c9121c9b2124427e140539a85a5f94bdf530001
AngelQuetzalC7 <gustavoquetzal944@gmail.com>
2025-05-13 19:11:35 -0600

    Entrega AA 3.2 proyecto CRUD API

---
+++ Unidad 3/3.2 Proyecto CRUD API/index.js
@@ -0,0 +1,96 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import Usuario from "./models/usuario.model.js";
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>{
+    res.send('Bienvenido en mi API CRUD');
+})
+
+app.post('/usuarios', async (req, res) => {
+    try {
+        const usuario = await Usuario.create(req.body);
+        res.status(201).json(usuario);
+    } catch (error){
+        console.error("Error al crear el usuario:", error);
+        res.status(500).json({error: "Error al crear el usuario"});
+    }
+});
+app.put('/usuarios/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const usuario = await Usuario.findByIdAndUpdate(id, req.body);
+        if(!usuario){
+            return res.status(404).json({error: 'Usuario no encontrado'});
+        }
+        const usActualizado = await Usuario.findById(id);
+        res.status(200).json(usActualizado);
+        console.log(usActualizado);
+    }catch (error){
+        console.error("Error al actualizar el usuario:", error);
+        res.status(500).json({error: "Error al actualizar el usuario"});
+    }
+});
+
+app.delete('/usuarios/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const usuario = await Usuario.findByIdAndDelete(id);
+        if(!usuario){
+            return res.status(404).json({error: 'Usuario no encontrado'});
+        }
+        res.status(200).json({message: 'Usuario eliminado'});
+    }catch{error}{
+        console.error("Error al eliminar el usuario:", error);
+        res.status(500).json({error: "Error al eliminar el usuario"});
+    }
+});
+
+app.get('/usuarios', async (req, res) => {
+    try{
+        const usuarios = await Usuario.find();
+        res.status(200).json(usuarios);
+    }catch (error){
+        console.error("Error al obtener los usuarios:", error);
+        res.status(500).json({error: "Error al obtener los usuarios"});
+    }
+});
+app.get('/usuarios/:id', async (req, res) => {
+    try{
+        const {id} = req.params;//Extrae el ID de los parametros de solicitud
+        const usuarios = await Usuario.findById(id);
+        res.status(200).json(usuarios);
+    }catch(error){
+        console.error("Error al obtener el ID:", error);
+        res.status(500).json({error: "Error al obtener el ID"});
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
+//define la URI 
+const uri = process.env.uri;
+
+//conectar a la base de datos
+mongoose.connect(uri)
+    .then(() => {
+        console.log("Conexion exitosa a la base de datos");
+    })
+    .catch ((error) => {
+        console.log("Error al conectar a la base de datos", error);
+    });
+
+    
